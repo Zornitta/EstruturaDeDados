ecf3d9919c3ecb49e2f8105de2efdd6db7a16bd4
Lucas Zornitta <429527@unoesc.lan>
2024-08-28 21:44:12 -0300

    Aula003

---
+++ Atividades/Aula02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using Aula02;
+
+Person person = new ();
+Console.WriteLine(person.Name);
+
+//Person 1:
+Person person1       
+    = new Person ("Mauricio Gonzatto", 40);
+Console.WriteLine(person1.Name);
+
+//Person 2:
+Person person2 = new ();
+person2.Name = "Vladmir Ilich Ulyanov";
+person2.Age = 70;
+Console.WriteLine(person2.Name);
+
+//Person 3:
+Person person3 = new ()
+{
+    Name = "Leonel Brizola",
+    Age = 80
+};
+Console.WriteLine(person3.Name);
