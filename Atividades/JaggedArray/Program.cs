03f80ec8e398bb2dad946345ee91c71a4190247e
Lucas Zornitta <429527@unoesc.lan>
2024-09-11 21:32:32 -0300

    Listas Genericas

---
+++ Atividades/JaggedArray/Program.cs
@@ -0,0 +1,22 @@
+﻿//JaggedArray é um vetor de vetores.
+
+int[][] numbers = int[4][];
+
+//Neste caso [][] sinaliza um vetor de vetores, neste exemplo um vetor de 4 posições contendo vetores de tamanho indefinido.
+
+numbers[0] = new int[] {1, 2, 3};
+numbers[1] = new int[] {4,3,2,1, 0};
+numbers[2] = new int[] {1,2, 0};
+numbers[3] = new int[] {57};
+numbers[4] = null!;
+
+//Obtenção de valor de JaggedArray
+
+int valor = numbers[0][2];
+Console.WriteLine(valor);
+
+//Atribuição de valor à JaggedArray:
+
+numbers[1][2] = 0;
+Console.WriteLine( numbers[1][2] );
+
---
+++ Atividades/ListaLivros/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Collections.Concurrent;
+using System.Runtime.Intrinsics.X86;
+using ListaLivros;
+
+List<Book> book = new List<Book>();
+
+
+for(int i=0; i<5; i++)
+{
+  Console.WriteLine(
+    $"Insira o nome dos livros: "
+);
+
+
+  string? nome = Console.ReadLine();
+  
+  Console.WriteLine(
+    $"Insira o nome da Editora do livro: "
+  );
+  
+  string? editora = Console.ReadLine();
+
+  Console.WriteLine(
+    $"Insira o nome do autor do livro: "
+  );
+
+  string? autor = Console.ReadLine();
+
+  Console.WriteLine(
+    $"Insira quantas páginas o livro possuí: "
+  );
+
+   int? paginas = Convert.ToInt32(Console.ReadLine());
+
+book.Add(new Book(){
+
+    Nome = nome,
+    Editora = editora,
+    Autor = autor,
+    Paginas = paginas,
+    }
+);
+}
+
+int maximo_paginas;
+
+for(int i=0; i<5; i++)
+{
+   if(maximo_paginas < )
+   {
+    maximo_paginas = ;
+   }
+}
